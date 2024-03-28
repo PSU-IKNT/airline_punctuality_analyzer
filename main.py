b257fba1cc34fe1c43b1864d60ea34cb12fd124e
MontelnV <vsor2005@gmail.com>
2024-03-28 10:49:44 +0500

    changes docs

--- main.py
+++ main.py
@@ -9,5 +9,21 @@ async def lifespan(app: FastAPI):
     print("App OFF")
     yield
 
-app = FastAPI(openapi_prefix="/api/v2/docs", lifespan=lifespan)
+description = """
+# AviaRatingAnalysisAPI - быстрый инструмент для анализа больших объемов данных
+и выстраивания рейтинга мировых авиакомпаний🚀
+
+## Возможности
+### - Построение рейтинга авиакомпаний на основе задержанных рейсов из дампов вашей базы данных
+### - Определение количества рейсов конкретных авиакомпаний в заданном аэропорту и их процент успешного выполнения
+"""
+
+app = FastAPI(openapi_prefix="/api/v2/docs", lifespan=lifespan,
+              title="AviaRatingAnalysisAPI",
+    description=description,
+    version="0.0.1",
+    license_info={
+        "name": "NGINX DOCS",
+        "url": "https://nginx.org/en/docs/",
+    })
 app.include_router(router)
--- routes.py
+++ routes.py
@@ -5,18 +5,19 @@ from repositories import AirlineOverAllRating
 from datetime import datetime, timedelta
 from overall_rating import run_overall_rating
 from database import create_tables, drop_tables
+from schemas import AirlineOverAllRatingResponse, FlightData
 
 router = APIRouter(
     tags=["Air"]
 )
 
-@router.get("/api/v2/airline_ratings_by_airports")
+@router.get("/api/v2/airline_ratings_by_airports", response_description= "Successful Response Airline Ratings by Airports", response_model=AirlineOverAllRatingResponse)
 async def get_airline():
     airline_ratings = await AirRepository.get_flight_data()
     return {"airline_ratings": airline_ratings}
 
-@router.get("/api/v2/overall_rating")
-async def get_airline():
+@router.get("/api/v2/overall_rating", response_description= "Successful Response Overall Rating", response_model=FlightData)
+async def get_airlines():
     last_update_date = await AirRepository.get_last_update_date()
     if last_update_date is not None:
 
